f3acbbcfde365561d4a1c754ae953563f7687820
YanKai <asd0989020308@gmail.com>
2019-02-05 15:45:52 +0800

    Finish second homework

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter number of ticket : ");
-        int numOfTicket = sc.nextInt();
-        System.out.print("Please enter number of round-ticket : ");
-        int numOfRoundTicket = sc.nextInt();
 
-        Ticket tk = new Ticket(numOfTicket,numOfRoundTicket);
-        tk.print();
+        int numOfTicket = 0, numOfRoundTicket = 0;
+
+        System.out.println("If you want to terminate this system , please enter -1.");
+
+        while(true) {
+                System.out.print("Please enter number of ticket : ");
+                numOfTicket = sc.nextInt();
+                if (numOfTicket == -1) break;
+                System.out.print("Please enter number of round-ticket : ");
+                numOfRoundTicket = sc.nextInt();
+                if(numOfRoundTicket == -1) break;
+                Ticket tk = new Ticket(numOfTicket,numOfRoundTicket);
+                tk.print();
+        }
+
+
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -5,13 +5,21 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
-    print("Please enter number of ticket : ")
-    var numOfTicket = sc.nextInt()
-    print("Please enter number of round-ticket : ")
-    var numOfRoundTicket = sc.nextInt()
 
-    val tk = Ticket(numOfTicket,numOfRoundTicket)
-    tk.print()
+    println("If you want to terminate this system , please enter -1.")
+
+    while(true) {
+        print("Please enter number of ticket : ")
+        var numOfTicket = sc.nextInt()
+        if(numOfTicket == -1) break
+        print("Please enter number of round-ticket : ")
+        var numOfRoundTicket = sc.nextInt()
+        if(numOfRoundTicket == -1) break
+
+        val tk = Ticket(numOfTicket,numOfRoundTicket)
+        tk.print()
+    }
+
 }
 
 class Ticket(var numOfTicket:Int,var numOfRoundTicket:Int){
@@ -19,6 +27,6 @@ class Ticket(var numOfTicket:Int,var numOfRoundTicket:Int){
     fun print(){
         var numOfNonRoundTicket = numOfTicket - numOfRoundTicket
         var sum = numOfNonRoundTicket * 1000 + numOfRoundTicket.toDouble() * 2000.0 * 0.9
-        print("Your total price is $" + sum.toInt() +" By Kotlin")
+        println("Your total price is $" + sum.toInt() +" By Kotlin")
     }
 }
